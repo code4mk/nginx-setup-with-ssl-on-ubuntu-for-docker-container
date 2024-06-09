63a7b2a104e0cb5908c5da36f80a6014b798ca57
Mostafa Kamal <hiremostafa@gmail.com>
2024-06-09 16:27:49 +0600

    permission and script

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# List available scripts
+echo "Available scripts:"
+echo "1. Docker deploy on Ubuntu"
+echo "2. Nginx install on Ubuntu"
+echo "3. Set SSL Nginx config"
+
+# Prompt user to select a script
+read -p "Enter the number of the script you want to run: " script_number
+
+case $script_number in
+    1)
+        # Execute docker-deploy-ubuntu.sh
+        bash script/docker-deploy-ubuntu.sh
+        ;;
+    2)
+        # Execute nginx-install-ubuntu.sh
+        bash script/nginx-install-ubuntu.sh
+        ;;
+    3)
+        # Execute set-ssl-nginx-conf.sh
+        bash script/set-ssl-nginx-conf.sh
+        ;;
+    *)
+        echo "Invalid selection. Please enter a valid script number."
+        ;;
+esac
